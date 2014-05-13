046c53a6ad9d05c6f92c80c0f042785e2297aeda
Germain Boue <boue.germain@gmail.com>
2014-05-13 10:02:58 -0700

    Created routes for juices + users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Juicebar::Application.routes.draw do
+  resources :users
+  resources :juices
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
